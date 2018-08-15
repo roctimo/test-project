1fd274a8bec8371d155582d7325abff20f0436db
roctimo <troche@cafemedia.com>
2018-08-15 11:58:17 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment' 
+import { environment } from './environments/environment';
 
-//bad comment
+// bad comment
 
 if (environment.production) {
   enableProdMode();
