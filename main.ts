cfb37a1533ab759d16602d0cd60caecd9b88becf
roctimo <troche@cafemedia.com>
2018-08-27 14:55:36 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ import { environment } from './environments/environment';
 // bad comment
 
 if (environment.production) {
-  enableProdMode()
+  enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
