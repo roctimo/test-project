941388c21fb3421e58c6971a00b2aeffcbcf39f8
roctimo <troche@cafemedia.com>
2018-09-10 17:15:18 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,10 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// bad comment
+//bad comment
 
 if (environment.production) {
-  enableProdMode();
+  enableProdMode()
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
